ae828c14b63566a57305de4de23fad36b94bbcd2
liane880218 <lianesuarez88@gmail.com>
2019-04-23 20:50:24 -0500

    Updates

---
+++ precache-manifest.069c80d2ac27ae246033799429a53bcb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae73efe935c286a8adc8",
+    "url": "/Clicky_Game/static/js/runtime~main.1f82a145.js"
+  },
+  {
+    "revision": "4c72d7b4c33fba4f4627",
+    "url": "/Clicky_Game/static/js/main.71ec143b.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/Clicky_Game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "4c72d7b4c33fba4f4627",
+    "url": "/Clicky_Game/static/css/main.d51e107e.chunk.css"
+  },
+  {
+    "revision": "b74f4d29f4783b0f3a4522386bf1130e",
+    "url": "/Clicky_Game/index.html"
+  }
+];
